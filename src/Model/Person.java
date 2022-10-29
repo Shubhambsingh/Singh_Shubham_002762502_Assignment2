8a77e18d9c6d7e760f4ff45c2240cfc964a8507c
Shubham Singh <singh.shubham1@northeastern.edu>
2022-10-29 16:16:36 -0400

    Doctor, Community, City and Vitals Pending

---
+++ src/Model/Person.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sirip
+ */
+public class Person {
+    private String name;
+    private double age;
+    private Patient patient;
+
+    public Person(String name, double age, Patient patient) {
+        this.name = name;
+        this.age = age;
+        this.patient = patient;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ src/Model/SystemAdmin/Login.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.SystemAdmin;
+
+/**
+ *
+ * @author sohamdeshpande
+ */
+public class Login {
+    private String Username;
+    private String Password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+  
+    
+}
